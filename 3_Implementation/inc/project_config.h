1ddf710cf5b270035f3e7db53ebb4c43b528fd1f
Shubham Sinha <94137581+Sinha4467@users.noreply.github.com>
2021-12-02 01:12:18 +0530

    Update project_config.h

--- 3_Implementation/inc/project_config.h
+++ 3_Implementation/inc/project_config.h
@@ -1,19 +1,6 @@
 #ifndef __PROJECT_CONFIG_H__
 #define __PROJECT_CONFIG_H__
-/**
- * @file project_config.h
- * @author Bharath.G ()
- * @brief Configuration file to define pins and ports for the interfaced peripherals
- * @version 0.1
- * @date 2021-04-23
- * 
- * @copyright Copyright (c) 2021
- * 
- */
 
-/**
- * Macro Definitions
- */
 
 #define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
 
