faf7c4a935184ac33260672d4ffff627db608e78
Shubham Sinha <94137581+Sinha4467@users.noreply.github.com>
2021-12-02 01:12:45 +0530

    Update user_utils.h

--- 3_Implementation/inc/user_utils.h
+++ 3_Implementation/inc/user_utils.h
@@ -1,15 +1,6 @@
 #ifndef __USER_UTILS_H__
 #define __USER_UTILS_H__
-/**
- * @file user_utils.h
- * @author Bharath.G ()
- * @brief User defined utilities used across the project
- * @version 0.1
- * @date 2021-04-23
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #include "project_config.h"
 #include <util/delay.h>
 
