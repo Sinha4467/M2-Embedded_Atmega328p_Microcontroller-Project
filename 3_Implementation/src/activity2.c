62decdc4b60f378f3f6ee902f81fba3e0c4dd133
Shubham Sinha <94137581+Sinha4467@users.noreply.github.com>
2021-12-02 01:13:57 +0530

    Update activity2.c

--- 3_Implementation/src/activity2.c
+++ 3_Implementation/src/activity2.c
@@ -1,12 +1,4 @@
-/**
- * @file Activity2.c
- * @author pavan
- * @ADC values 
- * @version 0.1
- * @date 2021-09-14
- * 
- * @copyright Copyright (c) 2021
- */
+
 #include "activity2.h"
 
 void initialiseADC()
